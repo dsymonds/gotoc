d5624fb4bf9a4aa4e1da88f8cb5acb264be8bc64
David Symonds <dsymonds@gmail.com>
2010-11-01 22:27:55 +1100

    Some parsing infrastructure; enough to parse the 'package foo;' line.

--- parser.go
+++ parser.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"fmt"
 	"io/ioutil"
 	"os"
 
@@ -14,27 +15,196 @@ func ParseFiles(filenames []string) (*FileDescriptorSet, os.Error) {
 	}
 
 	for i, filename := range filenames {
-		var err os.Error
-		fds.File[i], err = parseFile(filename)
+		fds.File[i] = &FileDescriptorProto{
+			Name: proto.String(filename),
+		}
+		buf, err := ioutil.ReadFile(filename)
 		if err != nil {
 			return nil, err
+		}
+
+		p := newParser(string(buf))
+		if pe := p.readFile(fds.File[i]); pe != nil {
+			return nil, pe
 		}
 	}
 
 	return fds, nil
 }
 
-func parseFile(filename string) (*FileDescriptorProto, os.Error) {
-	fd := &FileDescriptorProto{
-		Name: proto.String(filename),
+type parseError struct {
+	message string
+	line    int // 1-based line number
+	offset  int // 0-based byte offset from start of input
+}
+
+func (pe *parseError) String() string {
+	if pe.line == 1 {
+		return fmt.Sprintf("line 1.%d: %v", pe.offset, pe.message)
+	}
+	return fmt.Sprintf("line %d: %v", pe.line, pe.message)
+}
+
+type token struct {
+	value        string
+	err          *parseError
+	line, offset int
+}
+
+type parser struct {
+	s            string // remaining input
+	done         bool   // whether the parsing is finished
+	backed       bool   // whether back() was called
+	offset, line int
+	cur          token
+}
+
+func newParser(s string) *parser {
+	return &parser{
+		s:    s,
+		line: 1,
+		cur: token{
+			line: 1,
+		},
+	}
+}
+
+func (p *parser) readFile(fd *FileDescriptorProto) *parseError {
+	// Read package
+	if err := p.readToken("package"); err != nil {
+		return err
+	}
+	tok := p.next()
+	if tok.err != nil {
+		return tok.err
+	}
+	// TODO: check for a good package name
+	fd.Package = proto.String(tok.value)
+	if err := p.readToken(";"); err != nil {
+		return err
 	}
 
-	buf, err := ioutil.ReadFile(filename)
-	if err != nil {
-		return nil, err
+	// TODO: more
+
+	return nil
+}
+
+func (p *parser) readToken(expected string) *parseError {
+	tok := p.next()
+	if tok.err != nil {
+		return tok.err
+	}
+	if tok.value != expected {
+		return p.error("expected %q, found %q", expected, tok.value)
+	}
+	return nil
+}
+
+// Back off the parser by one token; may only be done between calls to next().
+func (p *parser) back() {
+	p.backed = true
+}
+
+// Advances the parser and returns the new current token.
+func (p *parser) next() *token {
+	if p.backed || p.done {
+		p.backed = false
+		return &p.cur
+	}
+	p.advance()
+	if p.done {
+		p.cur.value = ""
+	}
+	return &p.cur
+}
+
+func (p *parser) advance() {
+	// Skip whitespace
+	p.skipWhitespaceAndComments()
+	if p.done {
+		return
 	}
 
-	// TODO
-	_ = buf
-	return fd, nil
+	// Start of non-whitespace
+	p.cur.err = nil
+	p.cur.offset, p.cur.line = p.offset, p.line
+	switch p.s[0] {
+	// TODO: more cases, like punctuation.
+	case ';':
+		// Single symbol
+		p.cur.value, p.s = p.s[:1], p.s[1:]
+	default:
+		i := 0
+		for i < len(p.s) && isIdentOrNumberChar(p.s[i]) {
+			i++
+		}
+		if i == 0 {
+			p.error("unexpected byte %#x", p.s[0])
+			return
+		}
+		p.cur.value, p.s = p.s[:i], p.s[i:]
+	}
+	p.offset += len(p.cur.value)
+}
+
+func (p *parser) skipWhitespaceAndComments() {
+	i := 0
+	for i < len(p.s) {
+		if isWhitespace(p.s[i]) {
+			if p.s[i] == '\n' {
+				p.line++
+			}
+			i++
+			continue
+		}
+		if i+1 < len(p.s) && p.s[i] == '/' && p.s[i+1] == '/' {
+			// comment; skip to end of line or input
+			for i < len(p.s) && p.s[i] != '\n' {
+				i++
+			}
+			if i < len(p.s) {
+				// end of line; keep going
+				p.line++
+				continue
+			}
+			// end of input; fall out of loop
+		}
+		break
+	}
+	p.offset += i
+	p.s = p.s[i:]
+	if len(p.s) == 0 {
+		p.done = true
+	}
+}
+
+func (p *parser) error(format string, a ...interface{}) *parseError {
+	pe := &parseError{
+		message: fmt.Sprintf(format, a...),
+		line:    p.cur.line,
+		offset:  p.cur.offset,
+	}
+	p.cur.err = pe
+	p.done = true
+	return pe
+}
+
+func isWhitespace(c byte) bool {
+	// TODO: do better
+	return c == ' '
+}
+
+// Numbers and identifiers are matched by [-+._A-Za-z0-9]
+func isIdentOrNumberChar(c byte) bool {
+	switch {
+	case 'A' <= c && c <= 'Z', 'a' <= c && c <= 'z':
+		return true
+	case '0' <= c && c <= '9':
+		return true
+	}
+	switch c {
+	case '-', '+', '.', '_':
+		return true
+	}
+	return false
 }
